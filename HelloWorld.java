62ed4f8d1869dd3a7b069abb260ac11baec71b48
Todd838 <Toddperkins456@gmail.com>
2025-08-25 17:46:26 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+*Name: Todd Perkins
+*/
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	public static void main(String[] args){
+		System.out.pritnln("Hello World!!");
+	}
+}
